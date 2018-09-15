516425d1701384460a0ce73bffef3b2fc99902d8
Jean-Bertrand Couret <jb.couret@gmail.com>
2018-09-15 19:35:00 +0200

    create color variables & delete logo into navbar

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -8,22 +8,23 @@ import ProfileIcon from '../assets/icons/ic-person.svg'
 const Layout = ({ children }) => (
   <div className="App">
     <navbar className="navbar">
-      <div className="navbar__logo">JOBERAC</div>
-      <div className="navbar__search search">
-        <input type="text" className="search__field" placeholder="What are you looking for?" />
-        <button type="submit" className="search__button">
-          <img src={SearchIcon} alt=""/>
-        </button>
+      <div className="navbar__container-left">
+        <div className="navbar__search search">
+          <input type="text" className="search__field" placeholder="What are you looking for?" />
+          <button type="submit" className="search__button">
+            <img src={SearchIcon} alt=""/>
+          </button>
+        </div>
       </div>
-      <div className="navbar__buttons buttons">
-        <div className="buttons__button-company">
+      <div className="navbar__container-right">
+        <button className="navbar__container-right__button navbar__container-right__button-company">
           <img src={CompanyIcon} alt="" />
           <div>Entreprises</div>
-        </div>
-        <div className="buttons__button-profile">
+        </button>
+        <button className="navbar__container-right__button navbar__container-right__button-profile">
           <img src={ProfileIcon} alt=""/>
           <div>Profile</div>
-        </div>
+        </button>
       </div>
     </navbar>
     {children}
