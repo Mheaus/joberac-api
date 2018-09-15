666f85a57030e6e7bdb395b27cf0bb8e2a89280d
Jean-Bertrand Couret <jb.couret@gmail.com>
2018-09-15 18:17:28 +0200

    Create navbar base with logo, searchbar & icons

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import SearchIcon from '../assets/icons/ic-search.svg'
+import CompanyIcon from '../assets/icons/ic-business-center.svg'
+import ProfileIcon from '../assets/icons/ic-person.svg'
+
 const Layout = ({ children }) => (
   <div className="App">
-    <header className="App-header">
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
+    <navbar className="navbar">
+      <div className="navbar__logo">JOBERAC</div>
+      <div className="navbar__search search">
+        <input type="text" className="search__field" placeholder="What are you looking for?" />
+        <button type="submit" className="search__button">
+          <img src={SearchIcon} alt=""/>
+        </button>
+      </div>
+      <div className="navbar__buttons buttons">
+        <div className="buttons__button-company">
+          <img src={CompanyIcon} alt="" />
+          <div>Entreprises</div>
+        </div>
+        <div className="buttons__button-profile">
+          <img src={ProfileIcon} alt=""/>
+          <div>Profile</div>
+        </div>
+      </div>
+    </navbar>
     {children}
   </div>
 )
